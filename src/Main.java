2c9b88ae298883d5e72022c8eaf35c230b6d9e4e
Rumman93 <ahmmedjubayer2015@gmail.com>
2024-10-29 14:19:28 +0600

    project initialized

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Author :  Ahmmed Jubayer Rumman
+ * Created at : $DAY ${MONTH_NAME_SHORT} ${YEAR}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
